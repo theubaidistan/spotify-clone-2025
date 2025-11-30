89836e5065a735aebd3b6a8e0b3ac37aa62259ba
theubaidistan <ubaidistan395@gmail.com>
2025-11-30 19:02:12 +0500

    refactor: simplify cookie handling in getActiveProductsWithPrices function

--- actions/getActiveProductsWithPrices.ts
+++ actions/getActiveProductsWithPrices.ts
@@ -1,13 +1,39 @@
+// import { ProductWithPrice } from "@/types";
+// import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+// import { cookies } from "next/headers";
+
+// const getActiveProductsWithPrices = async (): Promise<ProductWithPrice[]> => {
+//   const cookieStore = await cookies();
+
+//   const supabase = createServerComponentClient({
+//     // cookies: cookies,
+//     cookies: () => cookieStore,
+//   });
+
+//   const { data, error } = await supabase
+//     .from("products")
+//     .select("*,prices(*)")
+//     .eq("active", true)
+//     .eq("prices.active", true)
+//     .order("metadata->index")
+//     .order("unit_amount", { foreignTable: "prices" });
+
+//   if (error) {
+//     console.log(error);
+//   }
+
+//   return (data as any) || [];
+// };
+
+// export default getActiveProductsWithPrices;
+
 import { ProductWithPrice } from "@/types";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
 const getActiveProductsWithPrices = async (): Promise<ProductWithPrice[]> => {
-  const cookieStore = await cookies();
-
   const supabase = createServerComponentClient({
-    // cookies: cookies,
-    cookies: () => cookieStore,
+    cookies, // pass the cookies function directly
   });
 
   const { data, error } = await supabase
